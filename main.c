a9151213e55b98b0d090b1f587d37e4dbd4e4843
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2021-08-07 19:15:39 +0300

    A bit more interactivity
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- main.c
+++ main.c
@@ -72,37 +72,87 @@ void list_menu_draw(struct list_menu *menu, WINDOW *window)
 	box(window, ACS_VLINE, ACS_HLINE);
 	mvwprintw(window, 0, 2, " %s ", menu->title);
 
-	for (i = 0; i < menu->item_count; ++i)
-		mvwprintw(window, 1 + i, 2, " %s ", menu->items[i]);
+	for (i = 0; i < menu->item_count; ++i) {
+		if (i == menu->current)
+			wattron(window, A_REVERSE);
+
+		mvwprintw(window, 2 + i, 2, " %s ", menu->items[i]);
+
+		if (i == menu->current)
+			wattroff(window, A_REVERSE);
+	}
 
 	wrefresh(window);
 }
 
-void list_menu_run(struct list_menu *menu, WINDOW *window)
+void list_menu_go_up(struct list_menu *menu)
 {
+	if (menu->current > 0)
+		--menu->current;
+}
+
+void list_menu_go_down(struct list_menu *menu)
+{
+	if (menu->current < menu->item_count - 1)
+		++menu->current;
+}
+
+int list_menu_run(struct list_menu *menu, WINDOW *window)
+{
+	while (true) {
+		int key;
+
+		list_menu_draw(menu, window);
+
+		key = wgetch(window);
+		switch (key) {
+			case 'k':
+			case KEY_UP:
+				list_menu_go_up(menu);
+				break;
+			case 'j':
+			case KEY_DOWN:
+				list_menu_go_down(menu);
+				break;
+			default:
+				return key;
+		}
+	}
+}
+
+void run_main_menu(WINDOW *menu_window)
+{
+	struct list_menu *main_menu;
+
+	main_menu = list_menu_new("coreboot configuration");
+	list_menu_add_item(main_menu, "(b)  Edit boot order");
+	list_menu_add_item(main_menu, "(o)  Edit options");
+	list_menu_add_item(main_menu, "(q)  Exit");
+
+	while (true) {
+		const int key = list_menu_run(main_menu, menu_window);
+		if (key == 'q')
+			break;
+	}
+
+	list_menu_free(main_menu);
 }
 
 int main(int argc, char **argv)
 {
 	WINDOW *menu_window;
-	struct list_menu *main_menu;
 
 	initscr();
 	noecho();
 	curs_set(false);
 
 	menu_window = newwin(getmaxy(stdscr), getmaxx(stdscr), 0, 0);
+	keypad(menu_window, true);
 
-	main_menu = list_menu_new("coreboot configuration");
-	list_menu_add_item(main_menu, "Edit boot order");
-	list_menu_add_item(main_menu, "Edit options");
-	list_menu_add_item(main_menu, "Exit");
+	run_main_menu(menu_window);
 
-	list_menu_draw(main_menu, menu_window);
+	delwin(menu_window);
 
-	sleep(2);
-
-	list_menu_free(main_menu);
 	endwin();
 }
 
